951a4cb8d2b79f1c6a19d5ced6576ea21363d20c
santiocampo <saocampor@unal.edu.co>
2022-10-13 15:52:30 -0500

    Modulos 2

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ interfaz.imprimirNombre("Santiago")
 interfaz.imprimirVariable("Número", [1,2,3])
 interfaz.separador("-")
 
-print(logica.sumar3Numeros(2,4,5))
-print(logica.sumarNumeros(1,2))
-print(logica.sumarListas([1,2,3],[3,2,1]))
+resultado1= logica.sumar3Numeros(2,4,5)
+resultado2= logica.sumarNumeros(1,2)
+resultado3= logica.sumarListas([1,2,3],[3,2,1])
+
+interfaz.imprimirVariable("Sumar 3 números", resultado1)
+interfaz.imprimirVariable("Sumar N números", resultado2)
+interfaz.imprimirVariable("Sumar 2 listas", resultado3)
