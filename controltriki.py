d572421f3f62c79b73b159ebc1ea43f6c3d26f8c
santiocampo <saocampor@unal.edu.co>
2022-10-27 15:01:38 -0500

    Triki

---
+++ controltriki.py
@@ -0,0 +1,23 @@
+import interfaztriki
+import logicatriki
+
+interfaztriki.explicarJuego()
+tableroJuego = logicatriki.obtenerTableroLogico()
+
+for turno in ["X", "O", "X", "O", "X", "O", "X", "O", "X"]:
+    print("\nTurno jugador", turno)
+    while True:
+        try:
+            posicion = int(input("Ingrese posicion de juego: "))
+        except 10:
+            continue
+        if (tableroJuego[posicion] == "X" or tableroJuego[posicion] == "O"):
+            print("Ingrese otra posicion")
+        else:
+            break
+    tableroJuego = logicatriki.actualizarTableroLogico(tableroJuego, posicion, turno)
+    interfaztriki.dibujarTablero(tableroJuego)
+    posibleGanador = logicatriki.determinarGanador(tableroJuego)
+    if posibleGanador in ["X", "O"]:
+        print("Felicidades, ha ganado el juego jugador", turno)
+        break
